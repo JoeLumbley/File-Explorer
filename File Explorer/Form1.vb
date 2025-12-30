695fc4980e27b592a93b072d09192052710a4d06
Joseph Lumbley <j_lumbley@hotmail.com>
2025-12-30 13:37:42 -0600

    Worked on Icons

--- File Explorer/Form1.vb
+++ File Explorer/Form1.vb
@@ -21,6 +21,9 @@
 ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ' SOFTWARE.
 
+' Github repo: https://github.com/JoeLumbley/File-Explorer
+
+
 Imports System.IO
 Imports System.Text.RegularExpressions
 
